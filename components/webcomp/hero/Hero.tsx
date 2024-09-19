4abb3fd2e8a6bd2efb7ad13ec5ba0c4e4d7fa47b
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-09-20 01:01:11 +0530

    feat ✨: Initial component implementations.
    - Added new Hero component with basic implementation.
    - Added initial Navigation component implementation.

---
+++ components/webcomp/hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero: React.FC = () => {
+  return (
+    <main>
+      <div>Hero</div>
+    </main>
+  );
+};
+
+export default Hero;
---
+++ components/webcomp/navigation/Nav.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const InterNav: React.FC = () => {
+  return (
+    <main>
+      <div>InterNav</div>
+    </main>
+  );
+};
+
+
+const Nav: React.FC = () => {
+  return (
+    <main>
+      <div>Nav</div>
+    </main>
+  );
+};
+
+export default Nav;
