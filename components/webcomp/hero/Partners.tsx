7a4164481957abb126cf841d20dab9ec71d7cbe3
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-09-21 01:23:28 +0530

    style 💎: Enable smooth scrolling and update UI styling for Partners component.
    - Added a new CSS rule to enable smooth scrolling on the HTML element.
    - Updated UI styling for the Partners component.

--- components/webcomp/hero/Partners.tsx
+++ components/webcomp/hero/Partners.tsx
@@ -12,8 +12,8 @@ const Partners: React.FC = () => {
 	return (
 		<main className={`sm:p-2 lg:p-4`}>
 			<div
-				className={`w-full mx-auto bg-slate-500 dark:bg-slate-300 bg-opacity-25 dark:bg-opacity-50 rounded-xl shadow-lg p-4`}>
-				Partners
+				className={`w-full mx-auto bg-slate-500 dark:bg-slate-400 bg-opacity-25 dark:bg-opacity-50 rounded-xl shadow-lg p-4`}>
+				<h1 className={`text-2xl font-bold underline`}>Our trusted electrical partners</h1>
 			</div>
 		</main>
 	);
