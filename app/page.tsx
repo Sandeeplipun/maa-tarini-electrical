ac00955979b2dc01d463d0614b32fa828fb9a52f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-09-21 14:31:29 +0530

    style 💎: Update responsive design elements and navigation bar styles.
    - Changed the background image style to cover instead of repeat.
    - Added padding to the main section for better layout.
    - Fixed top navigation bar styles.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Hero from "@/components/webcomp/landing/hero/Hero";
 
 export default function Home() {
 	return (
-		<main>
-			<div>
-				<Hero />
-			</div>
-			<div className={`pt-4`}>
-				<AboutLanding />
-			</div>
-		</main>
+			<main className={`pt-16`}> {/* Add padding-top equal to nav height */}
+				<div>
+					<Hero />
+				</div>
+				<div className={`pt-4`}>
+					<AboutLanding />
+				</div>
+			</main>
 	);
 }
--- components/webcomp/navigation/Nav.tsx
+++ components/webcomp/navigation/Nav.tsx
@@ -48,7 +48,7 @@ const Nav: React.FC = () => {
 	}, [] as typeof navItems);
 
 	return (
-		<nav className="border-b shadow-md">
+		<nav className="fixed top-0 left-0 right-0 z-50 bg-slate-100 dark:bg-gray-950 border-b shadow-md">
 			<div className="container mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center justify-between h-16">
 					<div className="flex items-center">
