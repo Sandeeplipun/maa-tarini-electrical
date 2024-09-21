1368a393628c8976d1155ba040f419f38e92dc6f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-09-21 14:47:00 +0530

    style 💎: Add consistent padding to main elements across pages.
    - Added padding to main element in about page.
    - Added padding to the contact page main element.
    - Added top padding to the main section of the page.
    - Added top padding to main element on shop page.

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const page: React.FC = () => {
   return (
-    <main>
+    <main className={`pt-16`}> {/* Add padding-top equal to nav height */}
       <div>page</div>
     </main>
   );
