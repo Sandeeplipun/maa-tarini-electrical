53bb6d73008091123eec72a277c9a7a07314eb8f
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-09-25 01:26:34 +0530

    feat ✨:  Update Intro.tsx

--- components/webcomp/about/Intro.tsx
+++ components/webcomp/about/Intro.tsx
@@ -3,8 +3,15 @@ import React from "react";
 const Intro: React.FC = () => {
 	return (
 		<main>
-			<div className={`bg-slate-500 dark:bg-slate-800 bg-opacity-50 p-4 shadow-xl`}>
-				<h1 className={`text-2xl md:text-3xl font-bold`}>About our store</h1>
+			<div
+				className={`bg-[linear-gradient(135deg,theme(colors.blue.400),theme(colors.purple.500))] dark:bg-[linear-gradient(135deg,theme(colors.blue.600),theme(colors.purple.700))] p-8 shadow-xl`}>
+				<h1 className={`text-2xl md:text-3xl text-center font-bold`}>
+					Welcome to our store
+				</h1>
+				<p className={`text-center text-sm md:text-base`}>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+					quos.
+				</p>
 			</div>
 		</main>
 	);
