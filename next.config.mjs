4b944da66186909f58e8683235a9eb049d15f564
SUDEEPTA SARKAR <sudsarkar13@gmail.com>
2024-10-25 18:46:42 +0530

    feat ✨: Support external HTTPS images
    - Configurations were made to support external HTTPS images at five specific hostnames plus HTTP support change from “remotePatterns and” updating feature set specifically onto web external service names addition via patterns features additions set .

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [""]
-  }
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				hostname: "www.bajajelectricals.com",
+				protocol: "https",
+				port: "",
+			},
+			{
+				hostname: "www.crompton.co.in",
+				protocol: "https",
+				port: "",
+			},
+			{
+				hostname: "in.shop.lighting.philips.com",
+				protocol: "https",
+				port: "",
+			},
+			{
+				hostname: "d32zxht0g2dn3w.cloudfront.net",
+				protocol: "https",
+				port: "",
+			},
+			{
+				hostname: "havells.com",
+				protocol: "https",
+				port: "",
+			},
+			{
+				hostname: "www.panasonic.com",
+				protocol: "https",
+				port: "",
+			},
+		],
+	},
 };
 
 export default nextConfig;
